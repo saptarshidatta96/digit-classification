1b71ccf91310a08fd533ce5285f3ba0136270055
Saptarshi Datta <saptarshidatta96@gmail.com>
2022-10-23 11:12:45 +0530

    quiz3

--- plot_digits_classification.py
+++ plot_digits_classification.py
@@ -12,11 +12,14 @@ hand-written digits, from 0-9.
 # License: BSD 3 clause
 
 # Standard scientific Python imports
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
 
 # Import datasets, classifiers and performance metrics
 from sklearn import datasets, svm, metrics
 from sklearn.model_selection import train_test_split
+import numpy as np
+import pytest
+import math
 
 ###############################################################################
 # Digits dataset
@@ -34,11 +37,7 @@ from sklearn.model_selection import train_test_split
 
 digits = datasets.load_digits()
 
-_, axes = plt.subplots(nrows=1, ncols=4, figsize=(10, 3))
-for ax, image, label in zip(axes, digits.images, digits.target):
-    ax.set_axis_off()
-    ax.imshow(image, cmap=plt.cm.gray_r, interpolation="nearest")
-    ax.set_title("Training: %i" % label)
+
 
 ###############################################################################
 # Classification
@@ -73,32 +72,26 @@ clf.fit(X_train, y_train)
 # Predict the value of the digit on the test subset
 predicted = clf.predict(X_test)
 
-###############################################################################
-# Below we visualize the first 4 test samples and show their predicted
-# digit value in the title.
+print(predicted)
 
-_, axes = plt.subplots(nrows=1, ncols=4, figsize=(10, 3))
-for ax, image, prediction in zip(axes, X_test, predicted):
-    ax.set_axis_off()
-    image = image.reshape(8, 8)
-    ax.imshow(image, cmap=plt.cm.gray_r, interpolation="nearest")
-    ax.set_title(f"Prediction: {prediction}")
 
-###############################################################################
-# :func:`~sklearn.metrics.classification_report` builds a text report showing
-# the main classification metrics.
 
 print(
     f"Classification report for classifier {clf}:\n"
     f"{metrics.classification_report(y_test, predicted)}\n"
 )
 
-###############################################################################
-# We can also plot a :ref:`confusion matrix <confusion_matrix>` of the
-# true digit values and the predicted digit values.
 
-disp = metrics.ConfusionMatrixDisplay.from_predictions(y_test, predicted)
-disp.figure_.suptitle("Confusion Matrix")
-print(f"Confusion matrix:\n{disp.confusion_matrix}")
 
-plt.show()
+
+def test_unbiased_classifier():
+    target_names = ['0', '1', '2', '3', '4', '5', '6', '6', '7', '8', '9']
+    cm = metrics.confusion_matrix(y_test, predicted)
+    cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+    print(cm)
+    acc = cm.diagonal()
+
+    #test case
+    assert np.any(acc < 0.5) == True
+
+
