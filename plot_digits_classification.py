2bc9c67da7f9f6775e106f9fe81697db7ad1e623
Saptarshi Datta <saptarshidatta96@gmail.com>
2022-11-01 18:10:34 +0530

    Assignment 4

--- plot_digits_classification.py
+++ plot_digits_classification.py
@@ -1,29 +1,128 @@
-# Author: Gael Varoquaux <gael dot varoquaux at normalesup dot org>
-# License: BSD 3 clause
 
-
-# PART: library dependencies -- sklear, torch, tensorflow, numpy, transformers
-
-# Import datasets, classifiers and performance metrics
 from poplib import CR
 from sklearn import datasets, svm, metrics, tree
 from itertools import product as pdt
 from joblib import dump, load
+from sklearn.model_selection import train_test_split
+import numpy as np
+from sklearn.metrics import accuracy_score as metric
 
-from utils import (
-    get_accuracy,
-    preprocess_digits,
-    train_dev_test_split,
-    h_param_tuning_svm,
-    #data_viz,
-    #pred_image_viz,
-    random_split_generator,
-    h_param_tuning_dect,
-    get_accuracy,
-    get_mean,
-    get_std,
-)
 
+def preprocess_digits(dataset):
+    n_samples = len(dataset.images)
+    data = dataset.images.reshape((n_samples, -1))
+    label = dataset.target
+    return data, label
+
+
+# random split generator
+def random_split_generator(num_sets):
+    train = []
+    dev = []
+    test = []
+    for i in range(0,num_sets):
+        train_dev_test = np.array(np.random.random(3))
+        train_dev_test /= train_dev_test.sum()
+        train.append(train_dev_test[0])
+        dev.append(train_dev_test[1])
+        test.append(train_dev_test[2])
+    return train, dev, test
+
+
+
+
+def train_dev_test_split(data, label, train_frac, dev_frac):
+
+    dev_test_frac = 1 - train_frac
+    x_train, x_dev_test, y_train, y_dev_test = train_test_split(
+        data, label, test_size=dev_test_frac, shuffle=True
+    )
+    x_test, x_dev, y_test, y_dev = train_test_split(
+        x_dev_test, y_dev_test, test_size=(dev_frac) / dev_test_frac, shuffle=True
+    )
+
+    return x_train, y_train, x_dev, y_dev, x_test, y_test
+
+
+def h_param_tuning_svm(h_param_comb, clf, x_train, y_train, x_dev, y_dev):
+    best_accuracy = -1.0
+    best_model = None
+    best_h_params = None
+    # 2. For every combination-of-hyper-parameter values
+    for Gamma,c in h_param_comb:
+
+        # PART: setting up hyperparameter
+        h_params = {'gamma':Gamma, 'C':c}
+        hyper_params = h_params
+        clf.set_params(**hyper_params)
+
+        # PART: Train model
+        # 2.a train the model
+        # Learn the digits on the train subset
+        clf.fit(x_train, y_train)
+
+        # PART: get dev set predictions
+        dev_prediction = clf.predict(x_dev)
+
+        # 2.b compute the accuracy on the validation set
+        model_accuracy = metric(y_dev, dev_prediction)
+
+        # 3. identify the combination-of-hyper-parameter for which validation set accuracy is the highest.
+        if model_accuracy > best_accuracy:
+            best_accuracy = model_accuracy
+            best_model = clf
+            best_h_params = h_params
+            print("Found new best metric for SVM with :" + str(h_params))
+            print("New best val metric for SVM:" + str(model_accuracy))
+    return best_model, best_accuracy, best_h_params
+
+
+
+
+def h_param_tuning_dect(h_param_comb, clf, x_train, y_train, x_dev, y_dev):
+    best_accuracy = -1.0
+    best_model = None
+    best_h_params = None
+    # 2. For every combination-of-hyper-parameter values
+    for Criterion, Splitter in h_param_comb:
+
+        # PART: setting up hyperparameter
+        h_params = {'criterion':Criterion, 'splitter':Splitter}
+        hyper_params = h_params
+        clf.set_params(**hyper_params)
+
+        # PART: Train model
+        # 2.a train the model
+        # Learn the digits on the train subset
+        clf.fit(x_train, y_train)
+
+        # print(cur_h_params)
+        # PART: get dev set predictions
+        dev_prediction = clf.predict(x_dev)
+
+        # 2.b compute the accuracy on the validation set
+        model_accuracy = metric(y_dev, dev_prediction)
+
+        # 3. identify the combination-of-hyper-parameter for which validation set accuracy is the highest.
+        if model_accuracy > best_accuracy:
+            best_accuracy = model_accuracy
+            best_model = clf
+            best_h_params = h_params
+            print("Found new best metric for Decision Tree Classifier with :" + str(h_params))
+            print("New best val metric for Decision Tree Classifier:" + str(model_accuracy))
+    return best_model, best_accuracy, best_h_params
+
+def get_accuracy(y_test, predicted):
+    accuracy = metric(y_test, predicted)
+    return accuracy
+
+
+def get_mean(arr):
+    _mean = np.mean(np.array(arr))
+    return _mean
+def get_std(arr):
+    _std = np.std(np.array(arr))
+    return _std
 
 
 train_fracs, dev_fracs, test_fracs = random_split_generator(5)
